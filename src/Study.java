992bceea472274cb228c4f3a03aa8ae53d27fe64
805-16 <805-16@DESKTOP-C7S4BG2>
2025-01-24 16:44:49 +0900

    xmncmxcn

---
+++ src/Study.java
@@ -0,0 +1,166 @@
+import java.util.Scanner;
+
+public class Study {
+
+	public static void main(String[] args) {
+//		int a=30;
+//		int b=12;
+//		
+//		int c=a+b;
+//		int d=a-b;
+//		int e=a*b;
+//		int x=a/b;
+//		int y=a%b;
+//		
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+//		System.out.println(x);
+//		System.out.println(y);
+
+		
+//		int a,b,c,d,e,f,g,h,i,j;
+//		a=1;
+//		b=2;
+//		c=3;
+//		d=4;
+//		e=5;
+//		f=6;
+//		g=7;
+//		h=8;
+//		i=9;
+//		j=10;
+//		
+//		int k;
+//		float l;
+//		long m;
+//		
+//		k=a*b*c*d*e*f*g*h*i*j;
+//		l=a*b*c*d*e*f*g*h*i*j;
+//		m=a*b*c*d*e*f*g*h*i*j;
+		
+		
+		
+//		int k=1+2+3+4+5+6+7+8+9+10;
+//		
+//		
+//		System.out.println("int값: "+k);
+//		System.out.println("float값: "+l);
+//		System.out.println("long값: "+m);
+//		System.out.println("테스트"+(1+2+3+4+5+6+7+8+9+10));
+		
+// 		살아온 날짜 계산
+		
+//		int birth, birthDate;
+//		
+//		birth=2024-1996;
+//		birthDate=birth*365;
+//		
+//		System.out.println(birth+"살");
+//		System.out.println(birthDate+"일");
+		
+				
+//		float k=24/3+2*5-3%2;
+//		
+//		System.out.println((int)k);
+		
+				
+//		int a=1;
+//		int b=-3;
+//		int c=2;
+//		float x1, x2;
+//		float root;
+//		
+//		root=(float)(Math.sqrt(b*b-4*a*c));
+//		x1=(-b+root)/2*a;
+//		x2=(-b-root)/2*a;
+//				
+//		System.out.println("x1 = "+x1);
+//		System.out.println("x2 = "+x2);
+		
+		// 입금액
+		
+//		int p=1000000;
+//		double r=0.03;
+//		int year=10;
+//		float rich;
+//		
+//		rich=p*(float)Math.pow((1+r), year);
+//				
+//		System.out.println(rich);
+//		
+
+//		float rich=1000000;
+//		double r=0.03;
+//		int year=10;
+//		
+//		System.out.println(rich+"는 원금");
+//				
+//		rich=rich*(float)Math.pow((1+r), year);
+//				
+//		System.out.println(rich+"는 불어난 금액");
+		
+		
+		
+		
+//		float ori=1000000;
+//		int year;
+//		double rate=0.03;
+//		float rich, rateMoney;
+//		
+//		Scanner in=new Scanner(System.in);
+//		
+//		System.out.print("확인을 원하는 연도를 입력하세요: ");
+//		year=in.nextInt();
+//		
+//		ori=ori*(float)Math.pow((1+(float)rate), year);
+//		rateMoney=year*(float)(ori*rate);
+//		
+//		System.out.println("이자금액은 "+rateMoney+"만원,");
+//		System.out.println("최종 금액은 "+ori+"만원입니다.");
+		
+		
+//		int x=0;
+//		
+//		x++;
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+//		x=x+1;		
+//		System.out.println("9x"+x+"="+(9*x));
+		
+		
+		int base=9;
+		int a=1;
+		int b=2;
+		int x=0;
+		
+		System.out.println(base+"x"+(x++)+"="+(base*x));
+		
+		System.out.println(base+"x"+(++x)+"="+(base*x));
+		
+		System.out.println(base+"x"+(++x)+"="+(base*x));
+		
+		System.out.println(base+"x"+(x++)+"="+(base*x));
+		
+		System.out.println(a+++b*a);
+		
+		System.out.println(++a+b*a);
+		
+		}
+
+}
---
+++ src/wth.java
@@ -0,0 +1,11 @@
+
+public class wth {
+
+	public static void main(String[] args) {
+		int a=10;
+		
+		System.out.println(a);
+
+	}
+
+}
